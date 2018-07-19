f90de21f5f92b4fe1c78bf55986f678a3f1c14fd
Travis <travis@devmaterial.com>
2018-07-19 05:33:03 -0400

    feat: display About footer nav item only on hompage

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const Footer = ({ siteTitle }) => (
-  <footer>
+const Footer = ({ siteTitle }) => {
+  let path = window.location.pathname;
+  return (
+    <footer>
     <a href="https://twitter.com/devmaterial">Development by <span>Travis Sanon</span></a>
-    <a className="center" href="/about">About</a>
+    <a className="center" href="/about" style={{ visibility: path === '/' ? 'visible' : 'hidden' }}>About</a>
     <a className="right" href="https://twitter.com/_micahcarroll">Design by <span>Micah Carroll</span></a>
   </footer>
-)
+  )
+}
 
 export default Footer
