6fcd9788d264cf413ceb279cb7c0904b85613230
travissanon <traviscsanon@gmail.com>
2021-02-24 14:30:21 -0500

    fix footer

--- src/templates/secondPage.js
+++ src/templates/secondPage.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import Layout from "../components/index"
-import { StaticQuery, graphql } from "gatsby"
+import Layout from '../components/index'
+import { StaticQuery, graphql } from 'gatsby'
 // import Img from "gatsby-image"
 
 import Header from '../components/header'
 import Footer from '../components/footer'
-import Divider from '../components/divider';
+import Divider from '../components/divider'
 
 const SecondPage = ({ location }) => (
-   <StaticQuery
-     query={graphql`
-       query People {
+  <StaticQuery
+    query={graphql`
+      query People {
         site {
           siteMetadata {
             title
@@ -51,37 +51,56 @@ const SecondPage = ({ location }) => (
             }
           }
         }
-       }
-     `}
-    
+      }
+    `}
     // let windowLocation = location.pathname.split("/").pop();
     render={data => (
       <Layout>
-        <Header/>
         <div className="list-content">
           <div className="list-content__container">
             <div className="list-content__header">
-              <h1>{data.site.siteMetadata.professionTitle[location.pathname.split("/").pop()]}</h1>
+              <h1>
+                {
+                  data.site.siteMetadata.professionTitle[
+                    location.pathname.split('/').pop()
+                  ]
+                }
+              </h1>
               {console.log(data.site.siteMetadata.professionTitle)}
-              <p className="body body--primary">{data.site.siteMetadata.professionDescription[location.pathname.split("/").pop()]}</p>
+              <p className="body body--primary">
+                {
+                  data.site.siteMetadata.professionDescription[
+                    location.pathname.split('/').pop()
+                  ]
+                }
+              </p>
             </div>
             <Divider />
-            <People data={data} location={location.pathname.split("/").pop()}/>
+            <People data={data} location={location.pathname.split('/').pop()} />
           </div>
         </div>
-        <Footer/>
       </Layout>
     )}
   />
 )
 
 const People = ({ data, location }) => {
-  let listOfPeople = [];
-  { data.allPeopleJson.edges.map(({ node }, i) => {
-    if (node.profession === location) {
-      listOfPeople.push(<Person person={node} image={data.allImageSharp.edges.filter(selection => selection.node.fluid.originalName === node.image)} key={i}/>)
-    }
-  }) }
+  let listOfPeople = []
+  {
+    data.allPeopleJson.edges.map(({ node }, i) => {
+      if (node.profession === location) {
+        listOfPeople.push(
+          <Person
+            person={node}
+            image={data.allImageSharp.edges.filter(
+              selection => selection.node.fluid.originalName === node.image
+            )}
+            key={i}
+          />
+        )
+      }
+    })
+  }
   return <div className="list-content__people">{listOfPeople}</div>
 }
 
@@ -95,7 +114,7 @@ const Person = ({ person, image, key }) => {
       <h2>{person.name}</h2>
       <p className="list-content__description">{person.description}</p>
     </div>
-  );
+  )
 }
 
 export default SecondPage
