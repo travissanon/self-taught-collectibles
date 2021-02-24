d7b386cd83221f39ca2e56d7760c99faf89946c8
travissanon <traviscsanon@gmail.com>
2021-02-24 14:30:21 -0500

    fix footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,27 @@
 import React from 'react'
-import Layout from "../components/index"
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
 import logo from './vector.svg'
 
-import Divider from '../components/divider';
-import Footer from '../components/footer';
+import Divider from '../components/divider'
+import Footer from '../components/footer'
 
 const IndexPage = () => (
-  <Layout>
-    <div className="home-content">
-      <div className="home-content__container">
-        <img src={logo}/>
-        <h1>Self Taught Collectibles</h1>
-        <p>
-          After years and years of favoriting and bookmarking youtubers 
-          I figured it was a matter of time before we created a archive of 
-          some of the highest quality content creators out there 
-          for both development and design.
-        </p>
-        <Divider/>
-        <Link to="/development">Development</Link>
-        <Link to="/design">Design</Link>
-      </div>
-      <Footer/>
+  <div className="home-content">
+    <div className="home-content__container">
+      <img src={logo} />
+      <h1>Self Taught Collectibles</h1>
+      <p>
+        After years and years of favoriting and bookmarking youtubers I figured
+        it was a matter of time before we created a archive of some of the
+        highest quality content creators out there for both development and
+        design.
+      </p>
+      <Divider />
+      <Link to="/development">Development</Link>
+      <Link to="/design">Design</Link>
     </div>
-  </Layout>
+    <Footer class="footer--home" />
+  </div>
 )
 
 export default IndexPage
